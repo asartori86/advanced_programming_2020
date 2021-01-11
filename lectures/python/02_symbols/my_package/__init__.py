0a28e2ef12ff5b57776b77a8038436b2b62076ce
Alberto Sartori <alberto.sartori86@gmail.com>
2021-01-11 11:03:53 +0100

    python lect 02

---
+++ lectures/python/02_symbols/my_package/__init__.py
@@ -0,0 +1,17 @@
+# it must be present
+
+# can be empty
+
+# or set the __all__ variable, which overrides *
+
+# __all__=['foo_module','bar_module']
+# __all__=['foo_module']
+
+# if the __all__ variable contains all the modules
+# do not define it
+
+
+# import my_package.bar_module
+
+# # from .bar_module import bar
+# from my_package.foo_module import foo
